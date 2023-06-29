9551fa7bb0974c942c8e65ef355fc81910710fc1
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-06-29 23:28:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+
+//Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden')
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+
+    // 3. Check for rolled 1: if true, switch to next player
+})
