7680f97a8971945762315f93dcb70d95531c2b84
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-07-04 22:39:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden')
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -33,7 +35,7 @@ btnRoll.addEventListener('click', function () {
     if (dice !== 1) {
         // Add dice to current score
         currentScore += dice;
-        current0El.textContent = currentScore; // Change Later 
+        document.getElementById(`current-{activePlayer}`);
     } else {
         // Switch to next player
 
