1b94eb42a839221d43088c40ad0c758b97e8d2c5
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-07-04 23:55:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,6 +52,8 @@ btnRoll.addEventListener('click', function () {
 btnHold.addEventListener('click', function () {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = scores[activePlayer];
 
     // 2. Check if player's score is >= 100
     // Finish the game
