4b82b479abc0a101bd52119b4fb5bd41f77dcaf6
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-07-04 23:09:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,5 +44,6 @@ btnRoll.addEventListener('click', function () {
         document.getElementById(`current--${activePlayer}`).textContent = 0;
         currentScore = 0;
         activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle() //toggle method will add if it is there or it will remove it if it is not there
     }
 })
