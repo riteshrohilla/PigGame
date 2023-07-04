40ab9334f3e4fc26be36e3907f48e546b6cfcfb5
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-07-04 23:05:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 // Selecting Elements
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
