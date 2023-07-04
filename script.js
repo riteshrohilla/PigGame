dd6b007094ca5e5808bb90445145f1874e962b73
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-07-04 23:57:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,16 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    //toggle method will add if it is there or it will remove it if it is not there
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
+}
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
     // 1. Generating a random dice roll
@@ -41,13 +51,8 @@ btnRoll.addEventListener('click', function () {
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         // Switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0El.classList.toggle('player--active'); //toggle method will add if it is there or it will remove it if it is not there
-        player1El.classList.toggle('player--active');
-    }
-})
+
+    })
 
 btnHold.addEventListener('click', function () {
     // 1. Add current score to active player's score
