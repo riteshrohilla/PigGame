1c911301f304d6a16975a1f27034c96b5512ac28
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-07-04 22:42:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,8 +36,6 @@ btnRoll.addEventListener('click', function () {
         // Add dice to current score
         currentScore += dice;
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-
-        current0El.textContent = currentScore;
     } else {
         // Switch to next player
 
