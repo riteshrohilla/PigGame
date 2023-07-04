0187996de59e24049f6467a459b2834fd5914806
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-07-04 22:56:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,8 @@ btnRoll.addEventListener('click', function () {
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
         // Switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
         activePlayer = activePlayer === 0 ? 1 : 0;
     }
 })
