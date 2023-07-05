3633ad215b572255cffaa1b89eee9987e476cd70
Ritesh Rohilla <riteshrohilla4@gmail.com>
2023-07-05 12:42:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,14 +16,23 @@ const btnHold = document.querySelector('.btn--hold');
 
 
 //Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden')
+const init = function () {
+    const scores = [0, 0];
+    let currentScore = 0;
+    let activePlayer = 0;
+    let playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden')
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -76,12 +85,5 @@ btnHold.addEventListener('click', function () {
 })
 
 btnNew.addEventListener('click', function () {
-    score0El.textContent = 0;
-    score1El.textContent = 0;
-    current0El.textContent = 0;
-    current1El.textContent = 0;
-    player0El.classList.remove('player--winner');
-    player1El.classList.remove('player--winner');
-    player0El.classList.add('player--active');
-    player1El.classList.remove('player--active');
+    init();
 })
